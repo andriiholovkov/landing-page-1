625c5176925ed8c7f729adb8f3a729f25d10d104
andriiholovkov <andriiholovkov@gmail.com>
2021-01-17 23:45:45 +0200

    implement header and second block

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,72 @@
-const { src, dest, series, watch } = require('gulp');
-const sass = require('gulp-sass');
-const csso = require('gulp-csso');
-const include = require('gulp-file-include');
-const del = require('del');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
-const sync = require('browser-sync').create();
+const { src, dest, series, watch } = require("gulp");
+const sass = require("gulp-sass");
+const csso = require("gulp-csso");
+const include = require("gulp-file-include");
+const del = require("del");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+const sync = require("browser-sync").create();
 
 function html() {
-  return src('app/**.html')
-    .pipe(include({
-      prefix: '@@'
-    }))
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(dest('dist'))
+  return src("app/**.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest("dist"));
 }
 
 function images() {
-  return src('app/images/**/*')
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
-        ]
-    })
-]))
-  .pipe(dest('dist/images'));
+  return src("app/images/**/*")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("dist/images"));
+}
+
+function svg() {
+  return src("app/svg/**/*")
+    .pipe(dest("dist/svg"));
 }
 
 function scss() {
-  return src('app/scss/**.scss')
+  return src("app/scss/**.scss")
     .pipe(sass())
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 2 version'],
-      cascade: false
-    }))
-    .pipe (csso())
-    .pipe(concat('style.css'))
-    .pipe(dest('dist'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 version"],
+        cascade: false,
+      })
+    )
+    .pipe(csso())
+    .pipe(concat("style.css"))
+    .pipe(dest("dist"));
 }
 
 function clear() {
-  return del('dist')
+  return del("dist");
 }
 
 function serve() {
   sync.init({
-    server: './dist'
-  })
+    server: "./dist",
+  });
 
-  watch('app/**html', series(html)).on('change', sync.reload)
-  watch('app/scss/**.scss', series(scss)).on('change', sync.reload)
+  watch("app/**html", series(html)).on("change", sync.reload);
+  watch("app/scss/**.scss", series(scss)).on("change", sync.reload);
 }
 
-exports.build = series(clear, scss, html, images)
-exports.serve = series(clear, scss, html, serve)
+exports.build = series(clear, scss, html, images, svg);
+exports.serve = series(clear, scss, html, images, svg, serve);
 exports.images = images;
 exports.clear = clear;
